693d34424e731484d21296823204ddf96d4fdf84
kinshuk <kinshuk364@gmail.com>
2024-06-24 16:21:59 +0200

    Admin Tools Added
    Press F5 to reload scene

---
+++ Assets/Scripts/AdminToolHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AdminToolHandler : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F5))
+        { ;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
