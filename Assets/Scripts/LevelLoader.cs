44675d8321ff88e37215f3c6c6c01b10f0a56f06
Ahmet Hakan Koç <ahmetkoc.business@gmail.com>
2024-06-26 18:44:42 +0200

    Update LevelLoader.cs

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -4,9 +4,14 @@ using DG.Tweening;
 using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour {
+    public static LevelLoader Ins { get; private set; }
     [SerializeField] private Image blackScreen;
     [SerializeField] private float fadeDuration;
     [SerializeField] private Levels nextLevelToLoad;
+
+    private void Awake() {
+        Ins = this;
+    }
 
     private void Start() {
         blackScreen.DOFade(0f, fadeDuration);
