f548ac909f7f0cda469b9d418a71bffb72f3c5ed
Ahmet Hakan Koç <ahmetkoc.business@gmail.com>
2024-07-05 17:22:34 +0200

    Dialogue event system WIP
    Work in progress. For now, start delay works

--- Assets/Scripts/DialogueEventSystem.cs
+++ Assets/Scripts/DialogueEventSystem.cs
@@ -1,7 +1,10 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class DialogueEventSystem : MonoBehaviour {
+    // TO DO: Implement proper end delay
+
     public static DialogueEventSystem Ins { get; private set; }
 
     [Header("Add whatever you want the game to do upon interacting with\n" +
@@ -9,11 +12,22 @@ public class DialogueEventSystem : MonoBehaviour {
             "position, destroying an object etc.")]
     [SerializeField] private UnityEvent onInteraction;
 
-    public UnityEvent OnInteraction {
-        get { return onInteraction; }
-    }
-
     private void Awake() {
         Ins = this;
     }
+
+    public void InvokeEvent(float startDelay = 0f, float endDelay = 0f) {
+        Invoke(nameof(InvokeEventActual), startDelay);
+        //if(endDelay > 0f) {
+        //    StartCoroutine(WaitGivenSeconds(endDelay));
+        //}
+    }
+
+    private void InvokeEventActual() {
+        onInteraction?.Invoke();
+    }
+
+    //private IEnumerator WaitGivenSeconds(float delay) {
+    //    yield return new WaitForSeconds(delay);
+    //}
 }
