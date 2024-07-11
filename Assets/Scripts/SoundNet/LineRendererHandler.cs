2b3660f013bf764928eca34bb8e7f865cacee241
kinshuk <kinshuk364@gmail.com>
2024-07-11 16:10:38 +0200

    Particle effect looking at the direction of mouse and visa versa is added

--- Assets/Scripts/SoundNet/LineRendererHandler.cs
+++ Assets/Scripts/SoundNet/LineRendererHandler.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using Unity.VisualScripting;
+
 [System.Serializable]
 public struct GameObjectPair
 {
