0ce0056712ed16cc403fb60d0d7803baa2265acc
kinshuk <kinshuk364@gmail.com>
2024-07-21 16:21:04 +0200

    Updated AdminTool
    -  press f1 + A to trigger game Manager level A, f1 + B to trigger game manager level B,...
    - delete button to reset to start menu
    -  press f1 + '+' / f1 + '-' to load the level one step higher or lower in the enum/build index list

--- Assets/Scripts/AdminToolHandler.cs
+++ Assets/Scripts/AdminToolHandler.cs
@@ -7,6 +7,39 @@ public class AdminToolHandler : MonoBehaviour
 {
     void Update()
     {
+        if (Input.GetKey(KeyCode.F1) && Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            // Load the next level
+            int nextLevelIndex = (SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings;
+            SceneManager.LoadScene(nextLevelIndex);
+        }
+
+        if (Input.GetKey(KeyCode.F1) && Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            // Load the previous level
+            int previousLevelIndex = (SceneManager.GetActiveScene().buildIndex - 1 + SceneManager.sceneCountInBuildSettings) % SceneManager.sceneCountInBuildSettings;
+            SceneManager.LoadScene(previousLevelIndex);
+        }
+        
+        if (Input.GetKey(KeyCode.F1) && Input.GetKeyDown(KeyCode.A))
+        {
+            // Load the next level specified by the NextLevelLayerA
+            GameManager.Ins.LoadNextLevel(GameManager.Ins.NextLevelLayerA);
+        }
+        
+        if (Input.GetKey(KeyCode.F1) && Input.GetKeyDown(KeyCode.B))
+        {
+            // Load the next level specified by the NextLevelLayerB
+
+            GameManager.Ins.LoadNextLevel(GameManager.Ins.NextLevelLayerB);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            //Load menu
+            SceneManager.LoadScene(0);
+        }
+        
         if (Input.GetKeyDown(KeyCode.F5))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
