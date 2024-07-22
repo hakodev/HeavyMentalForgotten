d2c031ba7a4fb10c3f5f7fd8ae09c76d3ad21967
kinshuk <kinshuk364@gmail.com>
2024-07-23 01:51:38 +0200

    AudioTriggerPrefab more functionality added for connectedOrb particularly

--- Assets/Scripts/SoundNet/AudioTriggerPrefab.cs
+++ Assets/Scripts/SoundNet/AudioTriggerPrefab.cs
@@ -2,20 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class AudioTriggerPrefab : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GameObject nextTriggerToActivate;
+    [SerializeField] private bool connectedDisconnetedTrigger;
+    public Light2D light2d;
     private AudioSource audioSource;
     private Collider2D[] colliders2D;
 
     [Header("SquareSettings")] 
     [SerializeField] private Vector3 squareDimensions;
     
-    
     [Header("Audio Clips")]
-    [SerializeField] private AudioClip audioClip;
+    [SerializeField] private AudioClip audioClipBothOrbs;
+    [SerializeField] private AudioClip audioClipConnectedOrb;
+    
+    [Header("Light settings")]
+    [SerializeField] private float maxIntensity;
+    [SerializeField] private float duration;
     
     private void Awake()
     {
@@ -36,40 +43,99 @@ public class AudioTriggerPrefab : MonoBehaviour
         colliders2D = Physics2D.OverlapBoxAll(center, squareDimensions, orientation.z);
 
         foreach (Collider2D collider in colliders2D)
-        {
-            if (collider.gameObject.CompareTag("SoundOrbDisconnected") || collider.gameObject.CompareTag("SoundOrbConnected"))
+        {                    
+
+            
+            // if (connectedDisconnetedTrigger)
+            // {
+                Debug.Log("Connected or disconnected trigger");
+                if (collider.gameObject.CompareTag("SoundOrbDisconnected") || collider.gameObject.CompareTag("SoundOrbConnected"))
+                {
+                    MemoryLayers memoryLayer;
+                    bool followMouse;
+
+                    if (collider.gameObject.CompareTag("SoundOrbDisconnected"))
+                    {
+                        var handler = collider.gameObject.GetComponent<DisconnectedSoundOrbHandler>();
+                        memoryLayer = handler.MemoryLayer;
+                        followMouse = handler.followMouse;
+                    }
+                    else //SoundOrbConnected
+                    {
+                        var handler = collider.gameObject.GetComponent<ConnectedSoundOrbHandler>();
+                        memoryLayer = handler.MemoryLayer;
+                        followMouse = handler.followMouse;
+                    }
+                
+                    if (audioSource != null && !audioSource.isPlaying && memoryLayer == MemoryLayers.C && followMouse)
+                    {
+                        audioSource.clip = audioClipBothOrbs;
+                        audioSource.Play();
+                        
+                        if (nextTriggerToActivate != null)
+                        {
+                            nextTriggerToActivate.SetActive(true);
+                        }
+                        // this.enabled = false;
+                        StartCoroutine(DisableScript(duration));
+                    }
+                    break;
+                }
+            // }
+
+            if (collider.gameObject.CompareTag("SoundOrbConnected"))
             {
                 MemoryLayers memoryLayer;
-                bool hovering;
-
-                if (collider.gameObject.CompareTag("SoundOrbDisconnected"))
-                {
-                    var handler = collider.gameObject.GetComponent<DisconnectedSoundOrbHandler>();
-                    memoryLayer = handler.MemoryLayer;
-                    hovering = handler.isHovering;
-                }
-                else //SoundOrbConnected
-                {
-                    var handler = collider.gameObject.GetComponent<ConnectedSoundOrbHandler>();
-                    memoryLayer = handler.MemoryLayer;
-                    hovering = handler.isHovering;
-                }
+                bool followMouse;
                 
-                if (audioSource != null && !audioSource.isPlaying && memoryLayer == MemoryLayers.C && hovering)
+                ConnectedSoundOrbHandler handler = collider.gameObject.GetComponent<ConnectedSoundOrbHandler>();
+                light2d = collider.gameObject.transform.GetChild(0).GetComponent<Light2D>();
+                memoryLayer = handler.MemoryLayer;
+                followMouse = handler.followMouse;
+                
+                if (audioSource != null && !audioSource.isPlaying && memoryLayer == MemoryLayers.C && followMouse)
                 {
-                    audioSource.clip = audioClip;
+                    StartCoroutine(ChangeLightIntensity(light2d, light2d.intensity, maxIntensity, duration));
+                    audioSource.clip = audioClipConnectedOrb;
                     audioSource.Play();
-                    //Add code to activate next trigger here
                     if (nextTriggerToActivate != null)
                     {
                         nextTriggerToActivate.SetActive(true);
                     }
-                    this.enabled = false;
                 }
-                break;
             }
             
         }
+    }
+    
+    private IEnumerator ChangeLightIntensity(Light2D light, float originalIntensity, float maxIntensity, float duration)
+    {
+        StartCoroutine(DisableScript(duration));
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration / 2)
+        {
+            light.intensity = Mathf.Lerp(originalIntensity, maxIntensity, elapsedTime / (duration / 2));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        while (elapsedTime < duration)
+        {
+            light.intensity = Mathf.Lerp(maxIntensity, originalIntensity, (elapsedTime - duration / 2) / (duration / 2));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        light.intensity = originalIntensity;
+
+    }
+    
+    private IEnumerator DisableScript(float time)
+    {
+        yield return new WaitForSeconds(time);
+        this.enabled = false;
+
     }
     
     private void OnDrawGizmos()
